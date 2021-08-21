6982894c10ba3ff209c49f7f4c9976c5db21fd8a
fuzzbuck <54221024+fuzzbuck@users.noreply.github.com>
2021-08-21 19:30:04 +0200

    stash unit drops mechanics

--- core/src/mindustry/td/TowerDefense.java
+++ core/src/mindustry/td/TowerDefense.java
@@ -1,12 +1,16 @@
 package mindustry.td;
 
 import arc.Events;
+import arc.math.Mathf;
 import arc.util.Log;
 import mindustry.*;
 import mindustry.content.*;
 import mindustry.game.EventType;
+import mindustry.game.Team;
 import mindustry.gen.*;
 import mindustry.net.*;
+import mindustry.type.Item;
+import mindustry.type.ItemStack;
 import mindustry.world.blocks.storage.*;
 
 import static mindustry.Vars.state;
@@ -41,6 +45,45 @@ public class TowerDefense {
             if (e.unit.team() == state.rules.waveTeam) e.unit.apply(StatusEffects.disarmed, Float.MAX_VALUE);
         });
 
+
+        Events.on(EventType.UnitDestroyEvent.class, e -> {
+            if (e.unit.team() == state.rules.waveTeam) {
+                Unit unit = e.unit;
+
+                ItemStack[] is = UnitDrops.drops.get(unit.type);
+                if(is ==  null || state.gameOver) return;
+
+                StringBuilder message = new StringBuilder();
+
+                for(ItemStack stack : is){
+                    Item item = stack.item;
+                    int amount = stack.amount;
+
+                    // TODO: fair drops (pvp compatible)
+                    /*
+                    if(item != null && unit) { // unit.target == core
+                        int calc = Mathf.random(amount - amount / 2, amount + amount / 2);
+                        message.append("[accent]+").append(calc).append("[] ").append(item.emoji()).append("  ");
+                        amount = depositCore.tile.build.acceptStack(item, calc, Team.sharded.core());
+                        if (amount > 0) {
+                            Call.transferItemTo(unit, item, amount, unit.x + Mathf.range(2f), unit.y + Mathf.range(2f), state.core);
+                        }
+                    }
+
+                     */
+                }
+                /*
+                String msg = message.toString();
+                for(Player p : Groups.player) {
+                    if(state.huds.containsKey(p.uuid()) && state.huds.get(p.uuid())) {
+                        Call.label(p.con, msg, Strings.stripColors(msg.replaceAll(" ", "")).length() / 14f, unit.x + Mathf.range(-2f, 2f), unit.y + Mathf.range(-2f, 2f));
+                    }
+                }
+                 */
+            }
+        });
+
         Log.info("TowerDefense inited");
     }
+
 }
