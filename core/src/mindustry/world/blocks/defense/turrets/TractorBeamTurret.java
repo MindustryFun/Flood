84362c9dacbc07cdf57bdbf9777b089b0cd5d512
fuzzbuck <54221024+fuzzbuck@users.noreply.github.com>
2021-08-21 19:30:08 +0200

    Update TractorBeamTurret.java

--- core/src/mindustry/world/blocks/defense/turrets/TractorBeamTurret.java
+++ core/src/mindustry/world/blocks/defense/turrets/TractorBeamTurret.java
@@ -127,7 +127,9 @@ public class TractorBeamTurret extends BaseTurret{
                     }
 
                     any = true;
-                    target.impulseNet(Tmp.v1.set(this).sub(target).limit((force + (1f - target.dst(this) / range) * scaledForce) * edelta() * timeScale));
+
+                    // save up on packets
+                    // target.impulseNet(Tmp.v1.set(this).sub(target).limit((force + (1f - target.dst(this) / range) * scaledForce) * edelta() * timeScale));
                 }
             }else{
                 strength = Mathf.lerpDelta(strength, 0, 0.1f);
